6a3c4cdb8f192256faffb08178fb8db92a0d78d8
mfatene <2023073@crosemont.qc.ca>
2023-11-14 09:43:34 -0500

    Implémentationde la fonctionnalité: Ouvrir l'application maps quand on click sur voir localisation

--- app/src/main/java/gti/g55/eventichs_organisation/Présentation/Vue/VueEvenement_detail.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/Présentation/Vue/VueEvenement_detail.kt
@@ -1,13 +1,17 @@
 package gti.g55.eventichs_organisation.Présentation.Vue
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import gti.g55.eventichs_organisation.Domaine.Entités.Évènement
@@ -22,6 +26,7 @@ class VueEvenement_detail : Fragment() {
     // TODO: Rename and change types of parameters
     lateinit var imagehome: ImageView
     lateinit var textViewNom: TextView
+    lateinit var versMaps:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +53,25 @@ class VueEvenement_detail : Fragment() {
         val selectedÉvènement = arguments?.getParcelable<Évènement>("Évènement")
         imagehome=view.findViewById(R.id.home)
         textViewNom = view.findViewById(R.id.nomEvent)
+        versMaps=view.findViewById(R.id.verxmaps)
+
+
+        versMaps.setOnClickListener {
+            val latitude = 45.55730452222236// specify the latitude of the location
+            val longitude = -73.5822902031885// specify the longitude of the location
+            val label = "Event Location" // specify the label for the location marker
+
+            val gmmIntentUri = "geo:$latitude,$longitude?q=$latitude,$longitude($label)"
+            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(gmmIntentUri))
+            mapIntent.setPackage("com.google.android.apps.maps")
+
+            if (mapIntent.resolveActivity(requireActivity().packageManager) != null) {
+                startActivity(mapIntent)
+            } else {
+                // Handle the case where Google Maps is not installed on the device
+                Toast.makeText(requireContext(), "Google Maps is not installed", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         imagehome.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.action_evenement_detail_to_evenement)
