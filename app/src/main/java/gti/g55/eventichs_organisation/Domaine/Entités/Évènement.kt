20a16272ce9169352e32e499650f8c3a0217b491
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-11-13 20:53:43 -0500

    Commit final pour merge avec dev

--- app/src/main/java/gti/g55/eventichs_organisation/Domaine/Entités/Évènement.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/Domaine/Entités/Évènement.kt
@@ -1,5 +1,5 @@
 package gti.g55.eventichs_organisation.Domaine.Entités
 
-class Évènement (val code: Int, val nom: String, val dateDebut: String, val dateFin: String, val type: String, val organisation_id: Int){
+class Évènement (val code: Int, val nom: String, val dateDebut: String, val dateFin: String, val description: String, val type: String, val organisation_id: Int){
 
 }
