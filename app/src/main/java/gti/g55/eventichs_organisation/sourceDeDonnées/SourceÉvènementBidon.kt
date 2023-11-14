b51a797c43d52d1e105f82b1c53fc50b1f3f62f8
mfatene <2023073@crosemont.qc.ca>
2023-11-14 11:39:08 -0500

    Changement des adresses à ceux qui se trouvent à montréal

--- app/src/main/java/gti/g55/eventichs_organisation/sourceDeDonnées/SourceÉvènementBidon.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/sourceDeDonnées/SourceÉvènementBidon.kt
@@ -19,7 +19,7 @@ class SourceÉvènementBidon : SourceÉvènement {
                         "21 Novembre 2023 à 08:00",
                         "23 Novembre 2023 à 08:00",
                         "Venez dénoncer la corruption du gouvernement qui choisit d'investir dans des choses inutiles en place de donner de l'argent aux professeurs!!!1!!",
-                        "85 Rue Saint-Paul, Québec, QC, G1K 3V8, Canada",
+                        "1235 Rue de la Montagne, Montréal, QC, H3G 1Z1, Canada",
                         "Public",
                         1
                     )
@@ -32,7 +32,7 @@ class SourceÉvènementBidon : SourceÉvènement {
                         "24 Novembre 2023 à 08:00",
                         "24 Novembre 2023 à 14:00",
                         "Je veux sentir jeune, pourquoi doit-on vieillir :(",
-                        "418 Rue Notre-Dame, Repentigny, QC, J6A 2T4, Canada",
+                        "360 Rue Saint-Antoine O, Montréal, QC, H2Y 3X4, Canada",
                         "Public",
                         1
                     )
@@ -45,7 +45,7 @@ class SourceÉvènementBidon : SourceÉvènement {
                         "15 NOVEMBRE 2023",
                         "15 NOVEMBRE 2023",
                         "FROM THE RIVER TO THE SEA, PALESTINE WILL BE FREE",
-                        "1500 Rue Royale, Trois-Rivières, QC, G9A 4J7, Canada",
+                        "7895 Boulevard Décarie, Montréal, QC, H4P 2H2, Canada",
                         "Public",
                         1
                     )
@@ -58,7 +58,7 @@ class SourceÉvènementBidon : SourceÉvènement {
                         "01-01-2023",
                         "08-08-3024",
                         "Piza",
-                        "555 Boulevard René-Lévesque E, Québec, QC, G1R 5S2, Canada",
+                        "2000 Avenue McGill College, Montréal, QC, H3A 3H3, Canada",
                         "Privée",
                         1
                     )
@@ -71,7 +71,7 @@ class SourceÉvènementBidon : SourceÉvènement {
                         "20 Février 2024",
                         "21 Février 2024",
                         "Savourer la viande de Meatman, des mains propres du Meatman lui-même!!",
-                        "555 Boulevard René-Lévesque E, Québec, QC, G1R 5S2, Canada",
+                        "55 Avenue du Mont-Royal O, Montréal, QC, H2T 2S6, Canada",
                         "Public",
                         1
                     )
