096b7d3a27a0b75b02287ecf77da35549378c9dd
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-11-27 00:06:57 -0500

    Log in Fonctionel sans le switch d'écran et aussi implémenté en MVP

--- app/src/main/java/gti/g55/eventichs_organisation/Présentation/Vue/VueLogin.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/Présentation/Vue/VueLogin.kt
@@ -1,6 +1,7 @@
 package gti.g55.eventichs_organisation.Présentation.Vue
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import gti.g55.eventichs_organisation.Présentation.Modèle.ModèleLogin
+import gti.g55.eventichs_organisation.Présentation.Présenteur.PrésentateurLogin
 import gti.g55.eventichs_organisation.R
+import gti.g55.eventichs_organisation.sourceDeDonnées.SourceUtilisateurBidon
+import org.w3c.dom.Text
 
 /**
  * A simple [Fragment] subclass.
@@ -17,9 +22,17 @@ import gti.g55.eventichs_organisation.R
  */
 class VueLogin : Fragment() {
 
+    private var _présenteur: PrésentateurLogin? = null
+
     lateinit var email: EditText
     lateinit var password: EditText
     lateinit var btnLogin: Button
+    lateinit var txtError: TextView
+
+
+    fun setPrésenteur(présentateurLogin: PrésentateurLogin){
+        _présenteur = présentateurLogin
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +54,25 @@ class VueLogin : Fragment() {
         email = view.findViewById(R.id.editEmail)
         password = view.findViewById(R.id.editPassword)
         btnLogin = view.findViewById(R.id.btnLogin)
+        txtError = view.findViewById(R.id.textErrorLogin)
 
         btnLogin.setOnClickListener {
-
+            _présenteur?.emailEtMotDePasseHandler(email.text.toString(), password.text.toString())
         }
+
+        val modèle = ModèleLogin(SourceUtilisateurBidon())
+        _présenteur = PrésentateurLogin(this, modèle)
+        
+        setPrésenteur(_présenteur!!)
+    }
+
+
+    fun goToNextFragment(){
+        Log.e("LOG", "all good")
+    }
+
+    fun messageErreur(){
+        txtError.text = "ERROR"
     }
 
 }
