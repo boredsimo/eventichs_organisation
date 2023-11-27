fcc9d883aac642044ca1561ff4c95be304d4cd5e
agascon <agasc@mon-ordi>
2023-11-27 00:32:03 -0500

    creer presque implémenté????

---
+++ app/src/main/java/gti/g55/eventichs_organisation/Présentation/Présenteur/PrésenteurCréation.kt
@@ -0,0 +1,77 @@
+package gti.g55.eventichs_organisation.Présentation.Présenteur
+
+import android.app.DatePickerDialog
+import android.os.Handler
+import android.os.Message
+import gti.g55.eventichs_organisation.Domaine.Entités.Évènement
+import gti.g55.eventichs_organisation.Domaine.Interacteurs.ÉvènementException
+import gti.g55.eventichs_organisation.Présentation.Modèle.ModèleVueEvenement
+import gti.g55.eventichs_organisation.Présentation.Vue.VueCreation
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class PrésenteurCréation (private val _vue: VueCreation, private val _modèle: ModèleVueEvenement){
+
+    private val handlerRéponse: Handler
+    private var filEsclave: Thread? = null
+    private val MSG_NOUVEL_EVENEMENT = 0
+    private val MSG_ERREUR = 1
+    private val MSG_ANNULER = 2
+
+    fun ajouterÉvènement(code: Int, nom: String, dateDebut: String, dateFin: String, description: String, addresse: String, type: String) {
+
+        filEsclave = Thread {
+            var msg: Message? = null
+            try {
+                Thread.sleep(0)
+                //temporaire
+                _modèle.CreerEvenement(
+                    Évènement(
+                        code,
+                        nom,
+                        dateDebut,
+                        dateFin,
+                        description,
+                        addresse,
+                        type,
+                        1
+                    )
+                )
+            } catch (e: ÉvènementException) {
+                msg = handlerRéponse.obtainMessage(MSG_ERREUR, e)
+            } catch (e: InterruptedException) {
+                msg = handlerRéponse.obtainMessage(MSG_ANNULER)
+            }
+
+            handlerRéponse.sendMessage(msg!!)
+        }
+        filEsclave!!.start()
+    }
+
+    fun changerDateDébut(date: DatePickerDialog) {
+        val currentDate = Calendar.getInstance()
+
+        date.datePicker.minDate = currentDate.timeInMillis
+
+        date.setOnDateSetListener { view, year, month, day ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, day)
+
+
+            _vue.changerDateDébut(SimpleDateFormat("MMMM d, yyyy", Locale.getDefault()).format(selectedDate))
+        }
+
+        date.show()
+    }
+
+    init {
+        handlerRéponse = object : Handler(){
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                filEsclave = null
+            }
+        }
+    }
+
+}
--- app/src/main/java/gti/g55/eventichs_organisation/Présentation/Vue/VueCreation.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/Présentation/Vue/VueCreation.kt
@@ -1,5 +1,6 @@
 package gti.g55.eventichs_organisation.Présentation.Vue
 
+import android.app.DatePickerDialog
 import androidx.fragment.app.Fragment
 import gti.g55.eventichs_organisation.R
 import android.os.Bundle
@@ -7,7 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 import androidx.navigation.Navigation
+import gti.g55.eventichs_organisation.Présentation.Présenteur.PrésenteurCréation
+import gti.g55.eventichs_organisation.sourceDeDonnées.SourceÉvènementBidon
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,31 +23,78 @@ private const val ARG_PARAM2 = "param2"
 
 class VueCreation : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    lateinit var btnVersAcceuil:Button
+    private var _présenteur: PrésenteurCréation? = null
+    private var fieldNomEvenement: EditText? = null
+    private var switchEvenementPrivé: Switch? = null
+    private var fieldAddresseEvenement: EditText? = null
+    private var fieldDescriptionEvenement: EditText? = null
+    private var btnDateDebut: Button? = null
+    private var btnDateFin: Button? = null
+    private var btnSubmit: Button? = null
+    private var textDateDebut: TextView? = null
+    private var textDateFin: TextView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var btnVersAcceuil: Button
+
+    val sourceBidon = SourceÉvènementBidon()
+
+    fun setPrésenteur(PrésenteurCréation: PrésenteurCréation?) {
+        _présenteur = PrésenteurCréation
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val vue = inflater.inflate(R.layout.fragment_creer, container, false)
+
+        fieldNomEvenement = vue.findViewById(R.id.fieldNomEvenement) // whyyy
+        switchEvenementPrivé = vue.findViewById(R.id.switchEvenementPrivé)
+        fieldAddresseEvenement = vue.findViewById(R.id.AddresseEvenement)
+        fieldDescriptionEvenement = vue.findViewById(R.id.DescriptionEvenement)
+        btnDateDebut = vue.findViewById(R.id.btnDateDebut)
+        textDateDebut = vue.findViewById(R.id.textDateDébut)
+        btnDateFin = vue.findViewById(R.id.btnDateFin)
+        textDateFin = vue.findViewById(R.id.textDateFin)
+        btnSubmit = vue.findViewById(R.id.btnSubmit)
+        btnVersAcceuil = vue.findViewById(R.id.btnRetourAcceuil)
+
+        btnVersAcceuil?.setOnClickListener {
+            Navigation.findNavController(vue).navigate(R.id.action_creerEvenement_to_evenement)
+        }
+
+
+        //calls the DatePickerDialog
+        //on date submit, it will modify
+        //btnSubmit?.setOnClickListener {
+        //    _présenteur?.ajouterÉvènement()
+        //}
+
+        btnDateDebut?.setOnClickListener {
+
+//            val currentDate = Calendar.getInstance()
+//            val DatePicker = DatePickerDialog(requireContext())
+//
+//            DatePicker.datePicker.minDate = currentDate.timeInMillis
+//
+//            DatePicker.setOnDateSetListener { DatePicker, year, month, day, ->
+//                val selectedDate = Calendar.getInstance()
+//                selectedDate.set(year, month, day)
+//
+//
+//            }
+
+            _présenteur?.changerDateDébut(DatePickerDialog(requireContext()))
+        }
+
+
         return inflater.inflate(R.layout.fragment_creer, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        btnVersAcceuil = view.findViewById(R.id.btnRetourAcceuil)
-        btnVersAcceuil.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_creerEvenement_to_evenement)
-        }
+    fun changerDateDébut(format: String) {
+        textDateDebut?.setText(format)
     }
 }
+
