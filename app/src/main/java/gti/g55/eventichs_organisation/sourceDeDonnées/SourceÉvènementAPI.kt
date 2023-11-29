cb79383e492dd48c9412b0b35e35c247856afbce
agascon <agasc@mon-ordi>
2023-11-29 01:53:55 -0500

    API Évènement, créer fonctionnel

--- app/src/main/java/gti/g55/eventichs_organisation/sourceDeDonnées/SourceÉvènementAPI.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/sourceDeDonnées/SourceÉvènementAPI.kt
@@ -6,10 +6,15 @@ import gti.g55.eventichs_organisation.Domaine.Entités.Évènement
 import gti.g55.eventichs_organisation.Domaine.Interacteurs.SourceÉvènement
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import okio.IOException
+import org.json.JSONObject
 
 class SourceÉvènementAPI: SourceÉvènement {
     override var listeRetour: ArrayList<Évènement> = ArrayList()
@@ -18,7 +23,6 @@ class SourceÉvènementAPI: SourceÉvènement {
     val client = OkHttpClient()
 
     override fun récupérerListeÉvènements(): List<Évènement> {
-        Log.i("Return body", "call started!")
         var request = Request.Builder()
             .url("http://v34l.com:8080/evenements")
             .build()
@@ -28,15 +32,13 @@ class SourceÉvènementAPI: SourceÉvènement {
 
 
             override fun onFailure(call: Call, e: IOException) {
-                Log.e("Return body", "Womp womp...")
-
+                Log.e("Return listeEvenement", "Womp womp")
             }
 
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
                     // Handle successful response here
                     val responseData = response.body?.string()
-                    Log.e("Return body", responseData.toString())
                     listeRetour = DécodeurJsonÉvénement.décoderJsonVersÉvénements(responseData.toString()) as ArrayList<Évènement>
                 } else {
                     // Handle unsuccessful response here
@@ -47,8 +49,43 @@ class SourceÉvènementAPI: SourceÉvènement {
         return listeRetour
     }
 
+
     override fun ajouterÉvènement(evenement: Évènement) {
-        TODO("Not yet implemented")
+        val JSON = "application/json; charset=utf-8".toMediaTypeOrNull()
+
+        val jsonObject = JSONObject()
+
+        jsonObject.put("id", evenement.code)
+        jsonObject.put("nom", evenement.nom)
+        jsonObject.put("adresse", evenement.addresse)
+        jsonObject.put("dateDebut", evenement.dateDebut)
+        jsonObject.put("dateFin", evenement.dateFin)
+        jsonObject.put("type", "public")
+        jsonObject.put("categorie", "Party")
+        jsonObject.put("description", evenement.description)
+        jsonObject.put("image", "explosion.jpg")
+        jsonObject.put("organisation", "Rosemont")
+
+        val requestBody = jsonObject.toString().toRequestBody(JSON)
+
+        val request = Request.Builder()
+            .url("http://v34l.com:8080/evenements")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("response", "fatal error caught")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    Log.e("response", response.code.toString())
+                } else {
+                    Log.e("response", response.code.toString())
+                }
+            }
+        })
     }
 
     override fun modifierÉvénements(Listecourante: List<Évènement>) {
