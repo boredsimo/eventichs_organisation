e5aae2b798784ee7b2dfa8f1743d7b49d5f4aeaa
Mahfu <mahfuzur.ali@hotmail.com>
2023-12-21 23:15:33 -0500

    Création du paramètre "light & dark mode".

--- app/src/main/java/gti/g55/eventichs_organisation/SettingsFragment.kt
+++ app/src/main/java/gti/g55/eventichs_organisation/SettingsFragment.kt
@@ -1,10 +1,15 @@
 package gti.g55.eventichs_organisation
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import java.util.prefs.AbstractPreferences
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,14 +23,11 @@ private const val ARG_PARAM2 = "param2"
  */
 class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var switch: Switch
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -37,23 +39,31 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        switch = view.findViewById(R.id.switch1)
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+        val savedThemeMode = sharedPreferences.getInt("theme_mode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        AppCompatDelegate.setDefaultNightMode(savedThemeMode)
+
+        switch.isChecked = (savedThemeMode == AppCompatDelegate.MODE_NIGHT_YES)
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (!isChecked) {
+                saveThemeMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                saveThemeMode(AppCompatDelegate.MODE_NIGHT_YES)
             }
+        }
     }
+    private fun saveThemeMode(themeMode: Int) {
+        with(sharedPreferences.edit()) {
+            putInt("theme_mode", themeMode)
+            apply()
+        }
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+    }
+
 }
